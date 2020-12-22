cd7e7394b55a8029c155fd12addc44bcd002971e
juanjts <1843@holbertonschool.com>
2020-12-22 22:56:01 +0000

    zero

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,13 @@
 # Web Server in web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
