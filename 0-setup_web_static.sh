dc5f20d58afd6b9920335ae168ee84e258b11b31
juanjts <1843@holbertonschool.com>
2020-12-22 23:01:51 +0000

    go back

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo chown -R ubuntu:ubuntu /data/
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
