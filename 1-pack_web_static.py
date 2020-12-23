983eb71d77c8856497c80b8191fa0ed9a82191e2
juanjts <1843@holbertonschool.com>
2020-12-23 01:51:14 +0000

    modofi

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive /
-from the contents of the web_static """
+"""Fabric script generates a .tgz archive from web_static"""
+from fabric.api import local
+import time
 
 
 def do_pack():
-    """ for generate .tgz """
-    from fabric.api import local
-    import time
+    """do_pack prototype to generate an .tgz"""
 
     local("mkdir -p versions")
     created = (time.strftime("%Y%m%d%H%M%S"))
-    compressed = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                       .format(created))
-    if not compressed.succeeded:
-        return "versions/web_static_{}.tgz".format(created)
-    else:
+    tgzfile = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                    .format(created))
+    if not tgzfile.succeeded:
         return None
+    else:
+        return "versions/web_static_{}.tgz".format(created)
