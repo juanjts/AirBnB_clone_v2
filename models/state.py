20e73f66658bda077e5fafccfe7214f017152714
juanjts <1843@holbertonschool.com>
2021-01-20 14:52:14 +0000

    state changed

--- models/state.py
+++ models/state.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
+from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states",
-                          cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """document"""
-        from models import storage
-        if getenv("HBNB_TYPE_STORAGE") != 'db':
-            """Get a list of all related City objects."""
-
-            cities_list = storage.all(City)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """function to return the cities list"""
+            cities_list = models.storage.all(City)
             all_cities = []
-            for value in cities_list.items():
-                for val in storage.all(State).items():
-                    if value.state_id == State.id:
-                        all_cities.append(value)
+            for city in cities_list.values():
+                if city.state_id == self.id:
+                    all_cities.append(city)
             return all_cities
