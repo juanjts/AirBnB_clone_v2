25e6c102769007ebe0503a98bc3b98ddd5b31d20
juanjts <1843@holbertonschool.com>
2021-01-21 03:11:23 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+""" Start Flask Web Application """
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown_db(self):
     """Call the storage.close() method"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def state_list():
     """display a HTML page"""
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    storage.close()
+    return (render_template("7-states_list.html",
+                            states=storage.all("State").values()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
